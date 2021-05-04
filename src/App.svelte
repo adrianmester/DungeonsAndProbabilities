61174d9cb3ba06437a61a8f63fa464cc4dd1d793
Adrian Mester <adrian.mester@numbrs.com>
2021-05-04 09:02:20 +0300

    add examples

--- src/App.svelte
+++ src/App.svelte
@@ -11,6 +11,12 @@
         formatTooltipX: d => (d + '').toUpperCase(),
         formatTooltipY: d => d + ' pts',
     }
+    let examples = [
+        ["d20+5", "Normal attack roll + 5"],
+        ["d20d", "Attack with disadvantage"],
+        ["d20+1d4", "Attack roll with Bless"],
+        ["5d20", "Just pretty"],
+    ]
 
     $: parseResult = parseDiceInputString(diceRollInput)
     $: dice = parseResult.dice;
@@ -31,26 +37,37 @@
         </div>
     </section>
     <div class="container" style="margin-top:60px">
-        <div class="field">
-            <label class="label" for="rollInput">Dice Roll</label>
-            <div class="control">
-                <input id="rollInput" class="input" type="text" bind:value={diceRollInput}>
+        <div class="columns">
+            <div class="column">
+                <div class="field">
+                    <label class="label" for="rollInput">Dice Roll</label>
+                    <div class="control">
+                        <input id="rollInput" class="input" type="text" bind:value={diceRollInput}>
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label" for="targetInput">Target Value</label>
+                    <div class="control">
+                        <input id="targetInput" class="input" bind:value={targetRollInput}>
+                    </div>
+                </div>
+                <p class="help is-danger">
+                    {#if error != null}
+                        {error}
+                    {/if}
+                </p>
+            </div>
+            <div class="column">
+                <div class="title is-5">Examples</div>
+                <div class="buttons">
+                    {#each examples as example}
+                        <button class="button is-link" on:click={()=>diceRollInput=example[0]}>{example[1]}</button>
+                    {/each}
+                </div>
             </div>
         </div>
-
-        <div class="field">
-            <label class="label" for="targetInput">Target Value</label>
-            <div class="control">
-                <input id="targetInput" class="input" type="number" bind:value={targetRollInput}>
-            </div>
-        </div>
-        <p class="help is-danger">
-            {#if error != null}
-                {error}
-            {/if}
-            &nbsp;
-        </p>
     </div>
+
     <div class="container chartContainer {chartContainerClass}">
         <Chart
                 data={displayedDice.chartData(parseInt(targetRollInput))}
